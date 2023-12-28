2794b76f532ffdf3d2163a3f069ebc37027e486a
QueenAgella <hervinfakhrulm@gmail.com>
2023-12-28 10:53:18 +0700

    PPL ManyChance

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login.index', [
+            'title' => 'Login',
+            'active' => 'login',
+
+            'login' => 'Silahkan Login',
+            'user' => 'Username',
+            'pass' => 'Password',
+
+            'logo' => 'sf.png',
+            'register' => 'Belum Punya Akun?',
+            'register2' => 'Daftar Disini',
+
+            'css' => 'login.css',
+            'js' => 'login.js'
+        ]);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->with('loginError', 'Login Failed!');
+
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+        return redirect('/');
+    }
+}
---
+++ resources/views/register/index.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/{{ $css }}">
+    <link href="{{ url('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css') }}" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>{{ $title }} | ManyChance</title>
+</head>
+<body>
+<div class="wrapper">
+    <div class="container main">
+        <div class="row">
+            <div class="col-md-6 side-image">
+
+                <!-------------      LOGO     ------------->
+
+                <img src="img/{{ $logo }}" alt="">
+                <div class="text">
+                    <p>Join the community of developers <i>- ludiflex</i></p>
+                </div>
+
+            </div>
+
+            <div class="col-md-6 right">
+
+                <div class="input-box">
+
+                    <header>{{ $create }}</header>
+                    <div class="input-field">
+                        <input type="text" class="input" id="name" required="" autocomplete="off">
+                        <label for="name">{{ $name }}</label>
+                    </div>
+                    <div class="input-field">
+                        <input type="text" class="input" id="username" required="" autocomplete="off">
+                        <label for="username">{{ $user }}</label>
+                    </div>
+                    <div class="input-field">
+                        <input type="text" class="input" id="email" required="" autocomplete="off">
+                        <label for="email">{{ $email }}</label>
+                    </div>
+                    <div class="input-field">
+                        <input type="password" class="input" id="password" required="">
+                        <label for="password">{{ $pass }}</label>
+                    </div>
+                    <div class="input-field">
+
+                        <input type="submit" class="submit" value="Login">
+                    </div>
+                    <div class="signin">
+                        <span>{{ $login }} <a href="/login">{{ $login2 }}</a></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
